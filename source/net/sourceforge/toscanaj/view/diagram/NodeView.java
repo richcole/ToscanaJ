1830e1ff946b8799c5a84d2ff8bcf26754dd613b
sgreen <sgreen@267469b6-68fb-4578-8a4b-5844dc4013a7>
2001-10-11 04:28:42 +0000

    NodeView now implements containsPoint() correctly.
    git-svn-id: svn://svn.code.sf.net/p/toscanaj/code/trunk@34 267469b6-68fb-4578-8a4b-5844dc4013a7

--- source/net/sourceforge/toscanaj/view/diagram/NodeView.java
+++ source/net/sourceforge/toscanaj/view/diagram/NodeView.java
@@ -12,11 +12,21 @@ import java.awt.geom.Point2D;
  */
 
 public class NodeView extends CanvasItem {
-
     /**
      * Store the node model for this view
      */
     private DiagramNode diagramNode = null;
+
+    /**
+     * Stores the grpahic environment.
+     *
+     * This is done since the radius in DiagramNode is not stored in model
+     * coordinates due to Toscanas weird scaling behaviour.
+     *
+     * @TODO: Reconsider the handling of the scaling or find a way to store
+     * the radius somewhere else (global options?).
+     */
+    private ToscanajGraphics2D graphics;
 
     /**
      * Construct a nodeView for a Node
@@ -25,20 +35,26 @@ public class NodeView extends CanvasItem {
         this.diagramNode = diagramNode;
     }
 
+    /**
+     * Draws the node as circle.
+     */
     public void draw(ToscanajGraphics2D g) {
         ///@TODO Probably should throw a NodeNotFoundException
         if(diagramNode != null) {
             g.drawEllipse2D(diagramNode.getPosition(), diagramNode.getRadius());
+            graphics = g;
         }
     }
 
     /**
      * Returns always false at the moment.
-     *
-     * @TODO: implement correct behaviour.
      */
     public boolean containsPoint(Point2D point) {
-        return false;
+        double deltaX = graphics.scaleX(point.getX() - diagramNode.getPosition().getX());
+        double deltaY = graphics.scaleY(point.getY() - diagramNode.getPosition().getY());
+        double sqDist = deltaX*deltaX + deltaY*deltaY;
+        double sqRadius = diagramNode.getRadius()*diagramNode.getRadius();
+        return sqDist <= sqRadius;
     }
 
     /**
