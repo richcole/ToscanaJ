c66c68580de33f19385f709ece518571bc19c8c7
peterbecker <peterbecker@267469b6-68fb-4578-8a4b-5844dc4013a7>
2001-10-26 01:48:22 +0000

    Added filter option to DataDump.
    The dump can now be restricted with an additional SQL clause like this:
      DataDump -filter "myCol = 'value'" myFile.csx
    
    
    git-svn-id: svn://svn.code.sf.net/p/toscanaj/code/trunk@154 267469b6-68fb-4578-8a4b-5844dc4013a7

--- source/net/sourceforge/toscanaj/DataDump.java
+++ source/net/sourceforge/toscanaj/DataDump.java
@@ -1,13 +1,20 @@
 package net.sourceforge.toscanaj;
 
+import net.sourceforge.toscanaj.controller.db.DBConnection;
 import net.sourceforge.toscanaj.model.ConceptualSchema;
 import net.sourceforge.toscanaj.model.diagram.Diagram2D;
+import net.sourceforge.toscanaj.model.diagram.DiagramLine;
+import net.sourceforge.toscanaj.model.diagram.DiagramNode;
+import net.sourceforge.toscanaj.model.diagram.SimpleLineDiagram;
+import net.sourceforge.toscanaj.model.lattice.AbstractConceptImplementation;
 import net.sourceforge.toscanaj.model.lattice.Concept;
+import net.sourceforge.toscanaj.model.lattice.DatabaseConnectedConcept;
 import net.sourceforge.toscanaj.parser.CSXParser;
 
 import java.io.File;
 import java.io.PrintStream;
 
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -23,15 +30,24 @@ import org.jdom.output.XMLOutputter;
  *
  * The call looks like this:
  *
- *   DataDump [-include-lists] filename.csx
+ *   DataDump [options] filename.csx
  *
- * And the dump will end up on stdout.
+ * And the dump will end up on stdout. Run "DataDump -help" for options.
+ *
+ * Return values:
+ * 0: everything went well
+ * 1: error in parameters
+ * 2: could not find/parse input
+ * 3: reserved
+ * 4: could not connect to database
+ * 5: could not write output
+ * 6: filter clause given for non-DB file
  */
 public class DataDump {
     /**
      * The main procedure.
      */
-    protected static void dumpData(File file, boolean includeLists) {
+    protected static void dumpData(File file, String filterClause, boolean includeLists) {
         // parse input
         ConceptualSchema schema = null;
         try {
@@ -40,13 +56,31 @@ public class DataDump {
         catch(Exception e) {
             System.err.println("Could not parse input.");
             e.printStackTrace();
-            return;
+            System.exit(2);
+        }
+        // create concept for filtering if needed
+        DatabaseConnectedConcept filterConcept = null;
+        if(filterClause != null) {
+            if(!schema.usesDatabase()) {
+                System.err.println("Filter can only applied to conceptual schemas that use a DB connection.");
+                System.exit(6);
+            }
+            try {
+                filterConcept = new DatabaseConnectedConcept(schema.getDatabaseInfo(),
+                                            new DBConnection(schema.getDatabaseInfo().getSource()) );
+            }
+            catch (Exception e) {
+                System.err.println("Couldn't create filter for database");
+                System.exit(4);
+            }
+            filterConcept.setObjectClause(filterClause);
         }
         // create output structure
         Document output = new Document(new Element("csxDump"));
         // dump all diagrams
         for(int diag = 0; diag < schema.getNumberOfDiagrams(); diag++) {
-            dumpDiagram(schema.getDiagram(diag), output.getRootElement(), includeLists);
+            dumpDiagram( filterDiagram(schema.getDiagram(diag), filterConcept),
+                         output.getRootElement(), includeLists);
         }
         // write XML to stdout
         XMLOutputter outputter = new XMLOutputter("    ", true);
@@ -56,7 +90,7 @@ public class DataDump {
         catch(Exception e) {
             System.err.println("Could not write output.");
             e.printStackTrace();
-            return;
+            System.exit(5);
         }
     }
 
@@ -127,6 +161,59 @@ public class DataDump {
     }
 
     /**
+     * Creates a filtered diagram.
+     *
+     * This is copy&paste code of DiagramController.getSimpleDiagram(int) -- we
+     * might want to get a common code base here. If the filterConcept is null,
+     * a copy of the input is returned.
+     */
+    protected static Diagram2D filterDiagram(Diagram2D inputDiagram, DatabaseConnectedConcept filterConcept) {
+        SimpleLineDiagram retVal = new SimpleLineDiagram();
+        Hashtable nodeMap = new Hashtable();
+
+        retVal.setTitle(inputDiagram.getTitle());
+        for(int i = 0; i<inputDiagram.getNumberOfNodes(); i++) {
+            DiagramNode oldNode = inputDiagram.getNode(i);
+            DiagramNode newNode;
+            if(filterConcept == null) {
+                newNode = new DiagramNode( oldNode.getPosition(),
+                                           oldNode.getConcept(),
+                                           oldNode.getAttributeLabelInfo(),
+                                           oldNode.getObjectLabelInfo() );
+            }
+            else {
+                newNode = new DiagramNode( oldNode.getPosition(),
+                                           oldNode.getConcept().filterByContingent(filterConcept),
+                                           oldNode.getAttributeLabelInfo(),
+                                           oldNode.getObjectLabelInfo() );
+            }
+            retVal.addNode(newNode);
+            nodeMap.put(oldNode,newNode);
+        }
+        for(int i = 0; i < inputDiagram.getNumberOfLines(); i++) {
+            DiagramLine line = inputDiagram.getLine(i);
+            DiagramNode from = (DiagramNode) nodeMap.get(line.getFromNode());
+            DiagramNode to = (DiagramNode) nodeMap.get(line.getToNode());
+            retVal.addLine( from, to );
+
+            // add direct neighbours to concepts
+            AbstractConceptImplementation concept1 =
+                             (AbstractConceptImplementation) from.getConcept();
+            AbstractConceptImplementation concept2 =
+                             (AbstractConceptImplementation) to.getConcept();
+            concept1.addSubConcept(concept2);
+            concept2.addSuperConcept(concept1);
+        }
+
+        // build transitive closures for each concept
+        for(int i = 0; i < retVal.getNumberOfNodes(); i++) {
+            ((AbstractConceptImplementation) retVal.getNode(i).getConcept()).buildClosures();
+        }
+
+        return retVal;
+    }
+
+    /**
      * Prints the usage information.
      *
      * @todo implement
@@ -135,6 +222,8 @@ public class DataDump {
         stream.println("Usage:");
         stream.println("  DataDump [options] filename.csx");
         stream.println("Options:");
+        stream.println("  -help / -? : print this message and exit");
+        stream.println("  -filter \"[SQL-Clause]\" : filter all queries with the given clause");
         stream.println("  -include-lists : include all lists of objects/attributes in the output");
     }
 
@@ -144,9 +233,23 @@ public class DataDump {
     public static void main(String[] args) {
         boolean includeLists = false;
         String filename = null;
+        String filterClause = null;
         for(int i = 0; i < args.length; i++ ) {
             if(args[i].equals("-include-lists")) {
                 includeLists = true;
+            }
+            else if(args[i].equals("-help") || args[i].equals("-?")) {
+                printUsage(System.out);
+                System.exit(0);
+            }
+            else if(args[i].equals("-filter")) {
+                if(i == args.length -1) {
+                    System.err.println("No filter clause given for '-filter' option");
+                    printUsage(System.err);
+                    System.exit(1);
+                }
+                i++;
+                filterClause = "(" + args[i] + ")";
             }
             else if(args[i].charAt(0)!='-') {
                 filename = args[i];
@@ -154,15 +257,15 @@ public class DataDump {
             else {
                 System.err.println("Unknown parameter");
                 printUsage(System.err);
-                return;
+                System.exit(1);
             }
         }
         if(filename == null) {
             System.err.println("No file name given");
             printUsage(System.err);
-            return;
+            System.exit(1);
         }
-        dumpData(new File(filename), includeLists);
-        return;
+        dumpData(new File(filename), filterClause, includeLists);
+        System.exit(0);
     }
 }
