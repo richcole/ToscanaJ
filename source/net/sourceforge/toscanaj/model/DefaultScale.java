b05c18af7784e798a7be9ed5fd26fb3b0b5e6ddb
rj-cole <rj-cole@267469b6-68fb-4578-8a4b-5844dc4013a7>
2002-06-26 21:52:40 +0000

    Start on Scales
    These scales are simply a beginning. Much work remains to be done.
    
    
    git-svn-id: svn://svn.code.sf.net/p/toscanaj/code/trunk@297 267469b6-68fb-4578-8a4b-5844dc4013a7

---
+++ source/net/sourceforge/toscanaj/model/DefaultScale.java
@@ -0,0 +1,23 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: rjcole
+ * Date: Jun 27, 2002
+ * Time: 5:55:54 AM
+ * To change template for new class use 
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package net.sourceforge.toscanaj.model;
+
+import concept.context.Context;
+import org.jdom.Element;
+
+public class DefaultScale implements Scale {
+    private Context context = new Context(0,0);
+
+    public void writeXML(Element elem) {
+        elem.addContent(new Element("Scale"));
+    }
+
+    public void readXML(Element elem) throws XML_SyntaxError {
+    }
+}
---
+++ source/net/sourceforge/toscanaj/model/Scale.java
@@ -0,0 +1,12 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: rjcole
+ * Date: Jun 27, 2002
+ * Time: 5:55:14 AM
+ * To change template for new interface use 
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package net.sourceforge.toscanaj.model;
+
+public interface Scale extends XML_Serializable {
+}
