3404235fadddc77335f26c554f5bb96bee2a0cfe
adrian82 <adrian82@267469b6-68fb-4578-8a4b-5844dc4013a7>
2002-08-23 10:56:10 +0000

    ToscanaJServlet Updated!!
    git-svn-id: svn://svn.code.sf.net/p/toscanaj/code/trunk@491 267469b6-68fb-4578-8a4b-5844dc4013a7

--- source/org/tockit/toscanaj/servlet/ToscanaJServlet.java
+++ source/org/tockit/toscanaj/servlet/ToscanaJServlet.java
@@ -9,10 +9,15 @@ package org.tockit.toscanaj.servlet;
 
 import net.sourceforge.toscanaj.parser.CSXParser;
 import net.sourceforge.toscanaj.model.ConceptualSchema;
+import net.sourceforge.toscanaj.model.database.*;
 import net.sourceforge.toscanaj.model.lattice.Concept;
 import net.sourceforge.toscanaj.model.lattice.Attribute;
 import net.sourceforge.toscanaj.model.diagram.*;
 import net.sourceforge.toscanaj.events.EventBroker;
+import net.sourceforge.toscanaj.controller.fca.*;
+import net.sourceforge.toscanaj.controller.db.DatabaseConnection;
+import net.sourceforge.toscanaj.controller.db.DatabaseException;
+import net.sourceforge.toscanaj.view.diagram.DiagramSchema;
 
 import java.io.*;
 import java.util.*;
@@ -20,24 +25,54 @@ import java.util.List;
 import java.awt.geom.Point2D;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.net.URL;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+/**
+ * @todo
+ */
 public class ToscanaJServlet extends HttpServlet {
     private static final String SERVLET_URL = "http://localhost:8080/toscanaj";
     private ConceptualSchema conceptualSchema = null;
+    private DatabaseConnectedConceptInterpreter conceptInterpreter = null;
     private static final int NODE_SIZE = 10;
     private static final Color NODE_COLOR = Color.blue;
     private static final Color LINE_COLOR = Color.black;
-    private static final int IMAGE_HEIGHT = 500;
-    private static final int IMAGE_WIDTH = 700;
+    private static final int IMAGE_HEIGHT = 600;
+    private static final int IMAGE_WIDTH = 800;
     private static final int FONT_SIZE = 12;
     private static final String FONT_FAMILY = "Arial";
+    private DiagramSchema diagramSchema = new DiagramSchema();
 
     public void init() throws ServletException {
         super.init();
         parseSchemaFile();
+        DatabaseInfo databaseInfo = conceptualSchema.getDatabaseInfo();
+        conceptInterpreter = new DatabaseConnectedConceptInterpreter(databaseInfo);
+        try {
+            DatabaseConnection connection = new DatabaseConnection(new EventBroker());
+            connection.connect(databaseInfo);
+            URL location = conceptualSchema.getDatabaseInfo().getEmbeddedSQLLocation();
+            if (location != null) {
+                connection.executeScript(location);
+            }
+            DatabaseConnection.setConnection(connection);
+        } catch (DatabaseException e) {
+            e.getOriginal().printStackTrace();
+            throw new ServletException("Could not connect to database", e);
+        }
     }
+
+    public void destroy() {
+        super.destroy();
+        try {
+            DatabaseConnection.getConnection().disconnect();
+        } catch (DatabaseException e) {
+            e.getOriginal().printStackTrace();
+        }
+    }
+
 
     protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
         doGet(httpServletRequest, httpServletResponse);
@@ -47,27 +82,96 @@ public class ToscanaJServlet extends HttpServlet {
     {
         PrintWriter out = resp.getWriter();
 
+        HttpSession session = req.getSession();
+
+        String conceptParameter = req.getParameter("concept");
+        String filterConceptParameter = req.getParameter("filterConcept");
         String diagramParameter = req.getParameter("diagram");
-        if(diagramParameter == null) {
+        DiagramHistory diagramHistory = (DiagramHistory) session.getAttribute("diagramHistory");
+        if(diagramHistory == null) {
+            diagramHistory = new DiagramHistory();
+            session.setAttribute("diagramHistory", diagramHistory);
+        }
+        if(filterConceptParameter != null) {
+            int diagramNumber = Integer.parseInt(diagramParameter);
+            diagramHistory.addDiagram(conceptualSchema.getDiagram(diagramNumber));
+            Concept concept = getConcept(diagramNumber, filterConceptParameter);
+            diagramHistory.next(concept);
             resp.setContentType("text/html");
             printDiagramList(out);
         }
-        else {
+        else if(conceptParameter != null) {
+            resp.setContentType("text/html");
+            int diagramNumber = Integer.parseInt(diagramParameter);
+            String queryParameter = req.getParameter("query");
+            Query query;
+            if(queryParameter != null) {
+                int queryNumber = Integer.parseInt(queryParameter);
+                query = (Query) conceptualSchema.getQueries().get(queryNumber);
+                session.setAttribute("query", query);
+            }
+            else {
+                query = (Query) session.getAttribute("query");
+                if(query == null) {
+                    query = (Query) conceptualSchema.getQueries().get(0);
+                }
+            }
+            printConceptList(diagramNumber, conceptParameter, query, diagramHistory, out);
+        }
+        else if(diagramParameter != null) {
             resp.setContentType("image/svg-xml");
             printHead(out);
             //printScript(out);
             int diagramNumber = Integer.parseInt(diagramParameter);
-            printDiagram(diagramNumber,out);
+            printDiagram(diagramNumber, diagramHistory, out);
             printEnd(out);
+        }
+        else {
+            resp.setContentType("text/html");
+            printDiagramList(out);
         }
     }
 
+    private void printConceptList(int diagramNumber, String nodeId, Query query, DiagramHistory diagramHistory, PrintWriter out) {
+        out.println("<html><head><title>ToscanaJServlet</title></head><body>");
+        out.println("<h1>Result of query '" + query.getName() + "':</h1>");
+        Concept concept = getConcept(diagramNumber, nodeId);
+        Iterator objIterator = conceptInterpreter.executeQuery(query,
+                                                    concept,
+                                                    new ConceptInterpretationContext(diagramHistory,new EventBroker())).iterator();
+        out.println("<ul>");
+        while (objIterator.hasNext()) {
+            String object = objIterator.next().toString();
+            out.println("<li>" + object + "</li>");
+        }
+        out.println("</ul>");
+        out.println("<h2>Change query:</h2>");
+        Iterator queryIterator = conceptualSchema.getQueries().iterator();
+        int i = 0;
+        out.println("<ol>");
+        while (queryIterator.hasNext()) {
+            Query curQuery = (Query) queryIterator.next();
+            out.println("<li><a href=\"" + SERVLET_URL + "?diagram=" + diagramNumber + "&amp;concept=" + nodeId +
+                        "&amp;query=" + i + "\">" + curQuery.getName() + "</a></li>");
+            i++;
+        }
+        out.println("</ol>");
+        out.println("</body></html>");
+    }
+
+    private Concept getConcept(int diagramNumber, String nodeId) {
+        Diagram2D diagram = conceptualSchema.getDiagram(diagramNumber);
+        DiagramNode node = diagram.getNode(nodeId);
+        Concept concept = node.getConcept();
+        return concept;
+    }
+
     private double addXPos(double oldPos) {
-        return (oldPos + 400);
+        return (oldPos + 350);
     }
 
     private double addYPos(double oldPos) {
-        return (oldPos + 200);
+        return (oldPos + 150);
     }
 
     /**
@@ -108,7 +212,7 @@ public class ToscanaJServlet extends HttpServlet {
         return fontMetrics.getHeight();
     }
 
-    private void printDiagram(int diagramNumber, PrintWriter out) {
+    private void printDiagram(int diagramNumber, DiagramHistory diagramHistory, PrintWriter out) {
         Diagram2D diagram = conceptualSchema.getDiagram(diagramNumber);
         int numLines = diagram.getNumberOfLines();
 
@@ -125,7 +229,7 @@ public class ToscanaJServlet extends HttpServlet {
             DiagramNode node = diagram.getNode(i);
             Point2D pos = node.getPosition();
             int radius = NODE_SIZE;
-            Color nodeColor = NODE_COLOR;
+            Color nodeColor = diagramSchema.getBottomColor();
             Color lineColor = LINE_COLOR;
             LabelInfo attrLabel = node.getAttributeLabelInfo();
 
@@ -134,8 +238,9 @@ public class ToscanaJServlet extends HttpServlet {
             Font font = new Font(FONT_FAMILY, Font.PLAIN, 12);
             FontMetrics fm = g2d.getFontMetrics(font);
 
-		    //out.println("<circle cx=\"" + addXPos(pos.getX()) + "\" cy=\"" + addYPos(pos.getY()) +"\" r=\"" + radius + "\" fill=\"blue\" stroke=\"black\" />");
+            out.println("<a xlink:href=\"" + SERVLET_URL + "?diagram=" + diagramNumber + "&amp;filterConcept=" + node.getIdentifier() + "\">");
             out.println("<circle cx=\"" + addXPos(pos.getX()) + "\" cy=\"" + addYPos(pos.getY()) +"\" r=\"" + radius + "\" style=\"fill:RGB(" + nodeColor.getRed() + "," + nodeColor.getGreen() + "," + nodeColor.getBlue() + ")\" stroke=\"black\" />");
+            out.println("</a>");
 
             if(attrLabel != null) {
                 // diagram related info (layout)
@@ -200,12 +305,29 @@ public class ToscanaJServlet extends HttpServlet {
                 Color textColor = objLabel.getTextColor();
                 int textAlignment = objLabel.getTextAlignment(); // do not use yet
                 Concept concept = node.getConcept();
-                Iterator objIt = concept.getObjectContingentIterator();
-                while (objIt.hasNext()) {
-                    //out.println("<text x=\"" + (pos.getX() + offset.getX()) + "\" y=\"" + (pos.getY() + offset.getY() - 15.0 ) + "\" font-family=\"Verdana\" fill=\"" + textColor.getRGB() + "\">");
-                    String object = objIt.next().toString();
-                    //out.println(object);
-                    //out.println("</text>");
+
+                int objectCount = conceptInterpreter.getObjectCount(
+                                                            concept,
+                                                            new ConceptInterpretationContext(diagramHistory, new EventBroker()));
+
+                if (objectCount != 0) {
+                    double maxLabelWidth = fm.stringWidth(String.valueOf(objectCount)) + 2 * fm.getLeading() + 2 * fm.getDescent();
+                    int maxLabelHeight = getHeight(fm);
+
+                    if ((textAlignment == 1) && (maxLabelWidth != 0.0)) {
+                        out.println("<a xlink:href=\"" + SERVLET_URL + "?concept=" + node.getIdentifier() +
+                                    "&amp;diagram=" + diagramNumber + "\" target=\"objectView\">");
+                        out.println("<rect width=\"" + maxLabelWidth + "\" height=\"" + maxLabelHeight + "\" x=\"" + (addXPos((pos.getX() + offset.getX())) - 3.0 - (0.5 * maxLabelWidth)) + "\" y=\"" + addYPos((pos.getY() + offset.getY() - 10.0 + maxLabelHeight )) + "\" style=\"fill:RGB(" + backgroundColor.getRed() + "," + backgroundColor.getGreen() + "," + backgroundColor.getBlue() +")\" stroke=\"Black\" />");
+                        drawLineConnector2(out, pos, offset, maxLabelWidth, maxLabelHeight, textAlignment);
+                        out.println("<text x=\"" + (addXPos(pos.getX() + offset.getX()) - (0.5 * maxLabelWidth))  + "\" y=\"" + addYPos((pos.getY() + NODE_SIZE + offset.getY() + 5.0)) + "\" font-size=\""+ FONT_SIZE + "\" font-family=\"" + FONT_FAMILY + "\" style=\"fill:RGB(" + textColor.getRed() + "," + textColor.getGreen() + "," + textColor.getBlue() +")\">");
+                        try {
+                            out.println(objectCount);
+                        } catch (Exception e) {
+                            e.printStackTrace(out);
+                        }
+                        out.println("</text>");
+                        out.println("</a>");
+                    }
                 }
             }
         }
@@ -220,6 +342,21 @@ public class ToscanaJServlet extends HttpServlet {
         }
         else if (labelWidth != 0.0) {
             out.println("<path d=\"M " + addXPos(centerOfNode.getX()) + " " + addYPos((centerOfNode.getY() - NODE_SIZE)) + " L " + ((addXPos((centerOfNode.getX() + offset.getX())) - 3.0) + ( 0.5 * ( 0.35 * labelWidth))) + " " + addYPos((centerOfNode.getY() + offset.getY() - 10.0)) + "\" stroke=\"black\" stroke-width=\"1\" />");
+        }
+        else {
+            // do not print the line connector
+        }
+    }
+
+    private void drawLineConnector2(PrintWriter out, Point2D centerOfNode, Point2D offset, double labelWidth, int labelHeight, int theTextAlignment) {
+        if ((theTextAlignment == 1) && (labelWidth != 0.0)) {
+            out.println("<path d=\"M " + addXPos(centerOfNode.getX()) + " " + addYPos((centerOfNode.getY() + NODE_SIZE)) + " L " + addXPos((centerOfNode.getX() + offset.getX())) + " " + addYPos((centerOfNode.getY() + NODE_SIZE - 5.0 + offset.getY())) + "\" stroke=\"black\" stroke-width=\"1\" />");
+        }
+        else if ((theTextAlignment == 2) && (labelWidth != 0.0)) {
+            out.println("<path d=\"M " + addXPos(centerOfNode.getX()) + " " + addYPos((centerOfNode.getY() + NODE_SIZE)) + " L " + (addXPos((centerOfNode.getX() + offset.getX())) - 3.0 - ( 0.5 * ( 0.65 * labelWidth))) + " " + addYPos((centerOfNode.getY() + offset.getY() - 10.0)) + "\" stroke=\"black\" stroke-width=\"1\" />");
+        }
+        else if (labelWidth != 0.0) {
+            out.println("<path d=\"M " + addXPos(centerOfNode.getX()) + " " + addYPos((centerOfNode.getY() + NODE_SIZE)) + " L " + ((addXPos((centerOfNode.getX() + offset.getX())) - 3.0) + ( 0.5 * ( 0.35 * labelWidth))) + " " + addYPos((centerOfNode.getY() + offset.getY() - 10.0)) + "\" stroke=\"black\" stroke-width=\"1\" />");
         }
         else {
             // do not print the line connector
@@ -279,26 +416,10 @@ public class ToscanaJServlet extends HttpServlet {
         }
     }
 
-//
-//    private void printScript(PrintWriter out) {
-//           out.println("<script type=\"text/ecmascript\"><![CDATA[");
-//           out.println("var svgDocument;");
-////           out.println("function adjustLabel(evt, int i, double x, double y) { ");
-////           out.println("evt.target.setAttribute(\"x\", x);");
-////           out.println("} ");
-//
-//           out.println("function initScript(evt) {");
-//           out.println("svgDocument = evt.getTarget().getOwnerDocument();");
-//           out.println("alert(svgDocument);");
-//           out.println("}");
-//
-//           out.println("]]></script>");
-//    }
-
     private void printHead(PrintWriter out) {
-        out.println("<?xml version=\"1.0\"?>");
+        out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
         out.println("<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.0//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\">");
-        out.println("<svg width=\"700\" height=\"500\" xmlns=\"http://www.w3.org/2000/svg\">");
+        out.println("<svg width=\"" + IMAGE_WIDTH + "\" height=\"" + IMAGE_HEIGHT + "\" xmlns=\"http://www.w3.org/2000/svg\">");
     }
 
     private void printEnd(PrintWriter out) {
