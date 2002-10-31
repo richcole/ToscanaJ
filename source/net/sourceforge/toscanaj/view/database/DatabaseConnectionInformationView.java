97f8b51fa33c827c83775868994f9db8b26b0d33
peterbecker <peterbecker@267469b6-68fb-4578-8a4b-5844dc4013a7>
2002-10-31 05:12:29 +0000

    Added button to generate URL for MDB file.
    git-svn-id: svn://svn.code.sf.net/p/toscanaj/code/trunk@554 267469b6-68fb-4578-8a4b-5844dc4013a7

--- source/net/sourceforge/toscanaj/view/database/DatabaseConnectionInformationView.java
+++ source/net/sourceforge/toscanaj/view/database/DatabaseConnectionInformationView.java
@@ -20,9 +20,11 @@ import org.tockit.events.EventBroker;
 import org.tockit.events.EventListener;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 /**
  * @todo the buttons are stupid, there should be a cancel button and an ok button, maybe "apply" as third
@@ -31,6 +33,8 @@ public class DatabaseConnectionInformationView extends JDialog implements EventL
     protected DatabaseInfo info;
 
     private DatabaseConnectEvent databaseConnectEvent;
+
+    private File openedDatabaseFile = null;
 
     private JTextField urlField;
     private JTextField userField;
@@ -51,7 +55,11 @@ public class DatabaseConnectionInformationView extends JDialog implements EventL
         super(frame, "Database connection");
         Container contentPane = this.getContentPane();
         contentPane.setLayout(new BorderLayout());
-        this.info = databaseInfo;
+        if (databaseInfo != null) {
+            this.info = databaseInfo;
+        } else {
+            this.info = new DatabaseInfo();
+        }
 
         JLabel urlLabel = new JLabel("Url:");
         JLabel driverLabel = new JLabel("Driver:");
@@ -63,8 +71,15 @@ public class DatabaseConnectionInformationView extends JDialog implements EventL
         userField = new JTextField();
         passwordField = new JTextField();
 
+        JButton fileButton = new JButton("File...");
+        fileButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                getFileURL();
+            }
+        });
+
         JButton connectButton = new JButton();
-        databaseConnectEvent = new DatabaseConnectEvent(this, databaseInfo);
+        databaseConnectEvent = new DatabaseConnectEvent(this, this.info);
         SimpleAction action = new SimpleAction(frame, "Connect");
         action.add(new SaveControlActivity());
         action.add(new EmitEventActivity(eventBroker, databaseConnectEvent));
@@ -78,16 +93,53 @@ public class DatabaseConnectionInformationView extends JDialog implements EventL
         });
 
         JPanel buttonPane = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        JPanel pane = new JPanel(new GridLayout(0, 2));
+        JPanel pane = new JPanel(new GridBagLayout());
 
-        pane.add(urlLabel);
-        pane.add(urlField);
-        pane.add(driverLabel);
-        pane.add(driverField);
-        pane.add(userLabel);
-        pane.add(userField);
-        pane.add(passwordLabel);
-        pane.add(passwordField);
+        pane.add(urlLabel, new GridBagConstraints(0, 0, 1, 1, 0, 0,
+                                                  GridBagConstraints.WEST,
+                                                  GridBagConstraints.NONE,
+                                                  new Insets(2,2,2,2),
+                                                  0, 0));
+        pane.add(urlField, new GridBagConstraints(1, 0, 1, 1, 1, 0,
+                                                  GridBagConstraints.CENTER,
+                                                  GridBagConstraints.BOTH,
+                                                  new Insets(2,2,2,2),
+                                                  0, 0));
+        pane.add(fileButton, new GridBagConstraints(2, 0, 1, 1, 0, 0,
+                                                  GridBagConstraints.EAST,
+                                                  GridBagConstraints.NONE,
+                                                  new Insets(2,2,2,2),
+                                                  0, 0));
+        pane.add(driverLabel, new GridBagConstraints(0, 1, 1, 1, 0, 0,
+                                                  GridBagConstraints.WEST,
+                                                  GridBagConstraints.NONE,
+                                                  new Insets(2,2,2,2),
+                                                  0, 0));
+        pane.add(driverField, new GridBagConstraints(1, 1, 1, 1, 1, 0,
+                                                  GridBagConstraints.CENTER,
+                                                  GridBagConstraints.BOTH,
+                                                  new Insets(2,2,2,2),
+                                                  0, 5));
+        pane.add(userLabel, new GridBagConstraints(0, 2, 1, 1, 0, 0,
+                                                  GridBagConstraints.WEST,
+                                                  GridBagConstraints.NONE,
+                                                  new Insets(2,2,2,2),
+                                                  0, 0));
+        pane.add(userField, new GridBagConstraints(1, 2, 1, 1, 1, 0,
+                                                  GridBagConstraints.CENTER,
+                                                  GridBagConstraints.BOTH,
+                                                  new Insets(2,2,2,2),
+                                                  0, 5));
+        pane.add(passwordLabel, new GridBagConstraints(0, 3, 1, 1, 0, 0,
+                                                  GridBagConstraints.WEST,
+                                                  GridBagConstraints.NONE,
+                                                  new Insets(2,2,2,2),
+                                                  0, 0));
+        pane.add(passwordField, new GridBagConstraints(1, 3, 1, 1, 1, 0,
+                                                  GridBagConstraints.CENTER,
+                                                  GridBagConstraints.BOTH,
+                                                  new Insets(2,2,2,2),
+                                                  0, 5));
 
         buttonPane.add(connectButton);
         buttonPane.add(closeButton);
@@ -99,6 +151,50 @@ public class DatabaseConnectionInformationView extends JDialog implements EventL
                 new Rectangle(100, 100, 300, 200));
 
         eventBroker.subscribe(this, DatabaseInfoChangedEvent.class, Object.class);
+    }
+
+    private void getFileURL() {
+        JFileChooser openDialog;
+        if (openedDatabaseFile != null) {
+            openDialog = new JFileChooser(openedDatabaseFile);
+        } else {
+            openDialog = new JFileChooser(System.getProperty("user.dir"));
+        }
+        openDialog.setFileFilter(new FileFilter() {
+            public boolean accept(File f) {
+                if( f.isDirectory() ) {
+                    return true;
+                }
+
+                String ext = "";
+                String s = f.getName();
+                int i = s.lastIndexOf('.');
+                if( i > 0 && i < s.length() - 1 ) {
+                    ext = s.substring(i+1).toLowerCase();
+                }
+                return ext.equals("mdb");
+            }
+
+            public String getDescription() {
+                return "Microsoft Access Databases";
+            }
+        });
+
+        if (openDialog.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+            openedDatabaseFile = openDialog.getSelectedFile();
+            // the following string is based on this: http://www.artima.com/legacy/answers/Feb2002/messages/141.html
+            String connectionURL = "jdbc:odbc:DRIVER=Microsoft Access Driver (*.mdb); " +
+                                   "DBQ=" + openedDatabaseFile.getAbsolutePath() + "; " +
+                                   "UserCommitSync=Yes; " +
+                                   "Threads=3; " +
+                                   "SafeTransactions=0; " +
+                                   "PageTimeout=5; " +
+                                   "MaxScanRows=8; " +
+                                   "MaxBufferSize=2048; " +
+                                   "DriverId=281";
+            urlField.setText(connectionURL);
+            driverField.setText("sun.jdbc.odbc.JdbcOdbcDriver");
+        }
     }
 
     public boolean areControlsChanged() {
