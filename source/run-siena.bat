1723151fe896b4bbab0a93df408af84446a3d352
peterbecker <peterbecker@267469b6-68fb-4578-8a4b-5844dc4013a7>
2003-01-14 05:08:58 +0000

    Added run scripts for the three editors.
    git-svn-id: svn://svn.code.sf.net/p/toscanaj/code/trunk@619 267469b6-68fb-4578-8a4b-5844dc4013a7

---
+++ source/run-siena.bat
@@ -0,0 +1,1 @@
+java -cp ToscanaJ.jar;libs/canvas.jar;libs/events.jar;libs/jdom.jar;libs/batik-awt-util.jar;libs/batik-dom.jar;libs/batik-svggen.jar;libs/batik-util.jar;libs/batik-xml.jar;libs/hsqldb.jar net.sourceforge.toscanaj.Siena
