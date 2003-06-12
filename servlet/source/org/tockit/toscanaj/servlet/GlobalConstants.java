b4451ee04ae4c465e67138318b866b4aaae6f04c
adrian82 <adrian82@267469b6-68fb-4578-8a4b-5844dc4013a7>
2003-06-12 02:03:59 +0000

    Added singleton class.
    git-svn-id: svn://svn.code.sf.net/p/toscanaj/code/trunk@1258 267469b6-68fb-4578-8a4b-5844dc4013a7

---
+++ servlet/source/org/tockit/toscanaj/servlet/GlobalConstants.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright DSTC Pty.Ltd. (http://www.dstc.com), Technische Universitaet Darmstadt
+ * (http://www.tu-darmstadt.de) and the University of Queensland (http://www.uq.edu.au).
+ * Please read licence.txt in the toplevel source directory for licensing information.
+ *
+ * $Id$
+ */
+package org.tockit.toscanaj.servlet;
+
+public class GlobalConstants {
+	static final int 	NODE_SIZE 		= 10;
+	static final int 	IMAGE_HEIGHT 	= 600;
+	static final int 	IMAGE_WIDTH 	= 800;
+	static final int 	FONT_SIZE 		= 12;
+	static final int 	OFFSET_X 		= 350;
+	static final int 	OFFSET_Y 		= 150;
+	static final String 	FONT_FAMILY 	= "Arial";
+	static final String 	PopupOptions 	= "toolbar=no," +
+		"location=no,directories=no,status=no,menubar=yes,scrollbars=yes," +
+		"resizable=no,copyhistory=yes,width=400,height=400";
+}
---
+++ servlet/source/org/tockit/toscanaj/servlet/GlobalVariables.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright DSTC Pty.Ltd. (http://www.dstc.com), Technische Universitaet Darmstadt
+ * (http://www.tu-darmstadt.de) and the University of Queensland (http://www.uq.edu.au).
+ * Please read licence.txt in the toplevel source directory for licensing information.
+ *
+ * $Id$
+ */
+package org.tockit.toscanaj.servlet;
+
+import net.sourceforge.toscanaj.model.database.DatabaseInfo;
+import net.sourceforge.toscanaj.model.ConceptualSchema;
+import net.sourceforge.toscanaj.controller.db.DatabaseConnection;
+import net.sourceforge.toscanaj.controller.db.DatabaseException;
+import net.sourceforge.toscanaj.controller.fca.DatabaseConnectedConceptInterpreter;
+import net.sourceforge.toscanaj.view.diagram.DiagramSchema;
+import net.sourceforge.toscanaj.parser.CSXParser;
+
+import org.tockit.events.EventBroker;
+
+import java.io.File;
+import java.net.URL;
+
+public class GlobalVariables {
+	
+	private static DiagramSchema diagramSchema = null;
+	private static ConceptualSchema conceptualSchema = null;
+	private static DatabaseConnectedConceptInterpreter conceptInterpreter = null;
+	private static String servletUrl;
+	
+	public static DiagramSchema getDiagramSchema() {
+		return diagramSchema;
+	}
+
+	public static ConceptualSchema getConceptualSchema() {
+		return conceptualSchema;
+	}
+
+	public static DatabaseConnectedConceptInterpreter 
+		getConceptInterpreter() {
+
+		return conceptInterpreter;
+	}
+
+	public static void initialize(File schemaFile, String servletUrl) {
+		GlobalVariables.servletUrl = servletUrl;
+		diagramSchema = new DiagramSchema();
+		try {
+			conceptualSchema = CSXParser.parse(new EventBroker(), schemaFile);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		DatabaseInfo databaseInfo = conceptualSchema.getDatabaseInfo();
+		conceptInterpreter = new DatabaseConnectedConceptInterpreter(databaseInfo);
+
+		try {
+			DatabaseConnection connection =
+				new DatabaseConnection(new EventBroker());
+			connection.connect(databaseInfo);
+			URL location =
+				conceptualSchema.getDatabaseInfo().getEmbeddedSQLLocation();
+			if (location != null) {
+				connection.executeScript(location);
+			}
+			DatabaseConnection.setConnection(connection);
+		} catch (DatabaseException e) {
+			e.printStackTrace();
+			throw new RuntimeException(
+				"Could not connect to database: " + e.getCause().getMessage(),
+				e);
+		}
+	}
+	
+	public static boolean isInitialized() {
+		return diagramSchema != null; 
+	}
+
+	public static String getServletUrl() {
+		return servletUrl;
+	}
+}
