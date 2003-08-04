5fb432501399af53bf3b014d1e1f03d9c0cecdf5
nataliya <nataliya@267469b6-68fb-4578-8a4b-5844dc4013a7>
2003-08-04 05:06:10 +0000

    Added temp code to read a file in N3 format and query it.
    Just trying out things at this stage.
    
    
    git-svn-id: svn://svn.code.sf.net/p/toscanaj/code/trunk@1338 267469b6-68fb-4578-8a4b-5844dc4013a7

--- tupleware/source/org/tockit/tupelware/rdf/query/RdqlQueryEngine.java
+++ tupleware/source/org/tockit/tupelware/rdf/query/RdqlQueryEngine.java
@@ -7,7 +7,103 @@
  */
 package org.tockit.tupelware.rdf.query;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.util.Iterator;
+import java.util.List;
+
+
+import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.n3.N3JenaReader;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFException;
+import com.hp.hpl.jena.rdf.model.RDFReader;
+import com.hp.hpl.jena.rdf.model.RDFReaderF;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.rdf.model.impl.RDFReaderFImpl;
+import com.hp.hpl.jena.rdql.Query;
+import com.hp.hpl.jena.rdql.QueryEngine;
+import com.hp.hpl.jena.rdql.QueryExecution;
+import com.hp.hpl.jena.rdql.QueryResults;
+import com.hp.hpl.jena.rdql.ResultBinding;
+
 
 public class RdqlQueryEngine {
+		
+	private RdqlQueryEngine () {	
+	}
+
+	public static void executeQuery(String queryString, Model model) {
+		Query query = new Query(queryString) ;
+		query.setSource(model);
+		
+		List queryVars = query.getResultVars();
+		System.out.println("Query vars: " + queryVars);
+		List triplePatterns = query.getTriplePatterns();
+		System.out.println("Triple patterns: " + triplePatterns);
+		Iterator it = triplePatterns.iterator();
+		while (it.hasNext()) {
+			Triple triple = (Triple) it.next();
+			System.out.println("triple pattern: " + triple);
+			System.out.println("\tobject: " + triple.getObject());
+			System.out.println("\tpredicate: " + triple.getPredicate());
+			System.out.println("\tsubject: " + triple.getSubject());
+		}
+		
+		QueryExecution qe = new QueryEngine(query) ;
+		QueryResults results = qe.exec() ;
+		List resultVars = results.getResultVars();
+		System.out.println("RESULT VARS: " + resultVars);
+		for ( Iterator iter = results ; iter.hasNext() ; ) {
+			ResultBinding cur = (ResultBinding)iter.next() ;
+			System.out.println("res binding: " + cur);
+			Object obj_A = cur.get("a") ;
+			// obj will be a Jena object: resource, property or RDFNode.
+			Object obj_B = cur.get("b") ;
+			Object obj_C = cur.get("c") ;
+			System.out.println("\t" + obj_A + "\timplements\t" + obj_C + "\tis-a\t" + obj_B);
+		}
+		results.close() ;
+	}
+	
+	public static void main (String[] args) {
+		if (args.length != 1 ) {
+			System.out.println("Expecting relative file path as an argument");
+			System.exit(0);
+		}
+		File file = new File(args[0]);
+		
+		String readerLang = "RDF/XML";
+		if (file.getAbsolutePath().endsWith(".n3")) {
+			readerLang = "N3";
+		}
+		
+		RDFReaderF rdfReaderFactory = new RDFReaderFImpl();
+		RDFReader rdfReader = rdfReaderFactory.getReader(readerLang);
+		
+		Model model = ModelFactory.createDefaultModel();
+		try {
+			rdfReader.read(model, file.toURL().toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		StmtIterator statementIterator = model.listStatements();
+		while(statementIterator.hasNext()) {
+			Statement curStatement = statementIterator.nextStatement();
+		}
+		
+		String queryString = "SELECT ?a, ?b " + 
+							"WHERE (?a, <implements>, ?c) , " + 
+							"(?c, <is-a>, ?b)";
+		System.out.println("Query: " + queryString);
+
+		RdqlQueryEngine.executeQuery(queryString, model);
+		
+		System.out.println("finished");
+	}
 
 }
