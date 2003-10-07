bba546aac16531e0e952781d82d0f51e27617ae7
peterbecker <peterbecker@267469b6-68fb-4578-8a4b-5844dc4013a7>
2003-10-07 04:03:51 +0000

    Fixed compareTo implementation.
    The old one produced too many equalities. It returned 0 for some objects
    that were not equal, thus losing objects in TreeSets.
    
    
    git-svn-id: svn://svn.code.sf.net/p/toscanaj/code/trunk@1479 267469b6-68fb-4578-8a4b-5844dc4013a7

--- source/net/sourceforge/toscanaj/model/context/FCAObjectImplementation.java
+++ source/net/sourceforge/toscanaj/model/context/FCAObjectImplementation.java
@@ -57,7 +57,11 @@ public class FCAObjectImplementation implements WritableFCAObject {
 			Comparable comparableData = (Comparable) this.data;
 			return comparableData.compareTo(other.getData());
 		}
-		return this.hashCode()/2 - other.hashCode()/2;
+        if(this.equals(other)) { // equals should return 0
+            return 0;
+        } else { // otherwise return some arbitrary but fixed non-zero value (using hashCode() might hit duplicates)
+		    return (int)((long)System.identityHashCode(this) - System.identityHashCode(other));
+        }
 	}
 	
 	public boolean equals(Object other) {
