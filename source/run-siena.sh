3aa6d6e4a3d1d4d8f442b1cd35c662525ecd6e09
peterbecker <peterbecker@267469b6-68fb-4578-8a4b-5844dc4013a7>
2003-10-10 01:42:49 +0000

    Fixed classpaths in manifest and start scripts.
    Added missing libraries.
    
    
    git-svn-id: svn://svn.code.sf.net/p/toscanaj/code/trunk@1497 267469b6-68fb-4578-8a4b-5844dc4013a7

--- source/run-siena.sh
+++ source/run-siena.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ulimit -s 2048
-java -cp ToscanaJ.jar:libs/canvas.jar:libs/events.jar:libs/jdom.jar:libs/batik-awt-util.jar:libs/batik-dom.jar:libs/batik-svggen.jar:libs/batik-util.jar:libs/batik-xml.jar:libs/hsqldb.jar:libs/iText.jar:libs/postscriptwriter.zip net.sourceforge.toscanaj.Siena
+java -cp ToscanaJ.jar:libs/batik-awt-util.jar:libs/batik-dom.jar:libs/batik-svggen.jar:libs/batik-util.jar:libs/batik-xml.jar:libs/canvas.jar:libs/events.jar:libs/freehep-pdfpatch.zip:libs/freehep-base.jar:libs/freehep-graphics2d.jar:libs/freehep-graphicsio-emf.jar:libs/freehep-graphicsio-pdf.jar:libs/freehep-graphicsio-ppm.jar:libs/freehep-graphicsio-ps.jar:libs/freehep-graphicsio.jar:libs/hsqldb.jar:libs/jdom.jar:libs/pluginArchitecture.jar:libs/relations.jar net.sourceforge.toscanaj.Siena
