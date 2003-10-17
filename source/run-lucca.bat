d5117b7888671bcc090ca5f078562c4899de3e0f
peterbecker <peterbecker@267469b6-68fb-4578-8a4b-5844dc4013a7>
2003-10-17 00:34:00 +0000

    Removed Lucca code.
    I don't think anyone is going to work on Lucca anytime soon, and there
    was not much done yet anyway. If work on it restarts things would have
    to be adjusted quite a bit to the changed code anyway.
    
    
    git-svn-id: svn://svn.code.sf.net/p/toscanaj/code/trunk@1525 267469b6-68fb-4578-8a4b-5844dc4013a7

--- source/run-lucca.bat
+++
@@ -1,1 +0,0 @@
-javaw -cp ToscanaJ.jar;libs/tockit-canvas.jar;libs/tockit-events.jar;libs/hsqldb.jar;libs/jdom.jar;libs/tockit-pluginArchitecture.jar;libs/tockit-relations.jar;libs/tockit-swing.jar net.sourceforge.toscanaj.Lucca
--- source/run-lucca.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ulimit -s 2048
-java -cp ToscanaJ.jar:libs/tockit-canvas.jar:libs/tockit-events.jar:libs/hsqldb.jar:libs/jdom.jar:libs/tockit-pluginArchitecture.jar:libs/tockit-relations.jar:libs/tockit-swing.jar net.sourceforge.toscanaj.Lucca
