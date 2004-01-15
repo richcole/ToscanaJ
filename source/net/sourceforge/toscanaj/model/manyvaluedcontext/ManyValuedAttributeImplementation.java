2ced008ce8e5c4c8bbfab2a2018f22c858a234cd
jhereth <jhereth@267469b6-68fb-4578-8a4b-5844dc4013a7>
2004-01-15 13:24:00 +0000

    Made the ManyValuedAttributeImplementation XMLizable
    git-svn-id: svn://svn.code.sf.net/p/toscanaj/code/trunk@1606 267469b6-68fb-4578-8a4b-5844dc4013a7

--- source/net/sourceforge/toscanaj/model/manyvaluedcontext/ManyValuedAttributeImplementation.java
+++ source/net/sourceforge/toscanaj/model/manyvaluedcontext/ManyValuedAttributeImplementation.java
@@ -7,10 +7,17 @@
  */
 package net.sourceforge.toscanaj.model.manyvaluedcontext;
 
+import org.jdom.Element;
+import net.sourceforge.toscanaj.model.manyvaluedcontext.types.TypeImplementation;
+import net.sourceforge.toscanaj.util.xmlize.XMLSyntaxError;
+import net.sourceforge.toscanaj.util.xmlize.XMLizable;
 
-public class ManyValuedAttributeImplementation implements WritableManyValuedAttribute {
+
+public class ManyValuedAttributeImplementation implements WritableManyValuedAttribute, XMLizable {
     private AttributeType type;
     private String name;
+	private static final String MANY_VALUED_ATTRIBUTE_ELEMENT_NAME = "manyValuedAttribute";
+	private static final String NAME_ATTRIBUTE_NAME = "name";
 
     public ManyValuedAttributeImplementation(AttributeType type, String name) {
         this.type = type;
@@ -35,5 +42,28 @@ public class ManyValuedAttributeImplementation implements WritableManyValuedAttr
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.sourceforge.toscanaj.util.xmlize.XMLizable#toXML()
+	 */
+	public Element toXML() {
+/*
+*     private AttributeType type;
+    private String name;
+
+ */		// TODO Auto-generated method stub
+		Element retVal = new Element(MANY_VALUED_ATTRIBUTE_ELEMENT_NAME);
+		retVal.setAttribute(NAME_ATTRIBUTE_NAME, name);
+		retVal.addContent(((TypeImplementation) type).toXML());
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.sourceforge.toscanaj.util.xmlize.XMLizable#readXML(org.jdom.Element)
+	 */
+	public void readXML(Element elem) throws XMLSyntaxError {
+		// TODO Auto-generated method stub
+		
 	}
 }
