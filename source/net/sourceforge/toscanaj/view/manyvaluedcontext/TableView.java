a6c09af152a4e8dfb9af4c67a4d27f1b5ebf443c
peterbecker <peterbecker@267469b6-68fb-4578-8a4b-5844dc4013a7>
2004-06-13 11:24:32 +0000

    Added basic cell editing capabilities for the many-valued context.
    git-svn-id: svn://svn.code.sf.net/p/toscanaj/code/trunk@1762 267469b6-68fb-4578-8a4b-5844dc4013a7

--- source/net/sourceforge/toscanaj/view/manyvaluedcontext/TableView.java
+++ source/net/sourceforge/toscanaj/view/manyvaluedcontext/TableView.java
@@ -7,14 +7,27 @@
  */
 package net.sourceforge.toscanaj.view.manyvaluedcontext;
 
+import java.text.NumberFormat;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
 import javax.swing.JTable;
+import javax.swing.event.TableModelEvent;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableColumnModel;
+import javax.swing.table.TableCellEditor;
+import javax.swing.text.NumberFormatter;
 
 import net.sourceforge.toscanaj.model.context.FCAElement;
+import net.sourceforge.toscanaj.model.manyvaluedcontext.AttributeType;
 import net.sourceforge.toscanaj.model.manyvaluedcontext.AttributeValue;
 import net.sourceforge.toscanaj.model.manyvaluedcontext.ManyValuedAttribute;
 import net.sourceforge.toscanaj.model.manyvaluedcontext.ManyValuedContext;
 import net.sourceforge.toscanaj.model.manyvaluedcontext.WritableManyValuedContext;
+import net.sourceforge.toscanaj.model.manyvaluedcontext.types.NumericalType;
+import net.sourceforge.toscanaj.model.manyvaluedcontext.types.NumericalValue;
+import net.sourceforge.toscanaj.model.manyvaluedcontext.types.TextualType;
 
 /**
  * A view for many-valued contexts.
@@ -27,23 +40,20 @@ import net.sourceforge.toscanaj.model.manyvaluedcontext.WritableManyValuedContex
  * the many-valued context, not a mapping.
  */
 public class TableView extends JTable {
-	private class ContextTableModel extends AbstractTableModel {
-		private ManyValuedContext context;
+	private ManyValuedContext context;
 
-		public ContextTableModel(ManyValuedContext context) {
-			this.context = context;
-		}
+	private class ContextTableModel extends AbstractTableModel {
 
 		public int getColumnCount() {
-			return this.context.getAttributes().size();
+			return context.getAttributes().size();
 		}
 
 		public int getRowCount() {
-			return this.context.getObjects().size();
+			return context.getObjects().size();
 		}
 
 		public boolean isCellEditable(int rowIndex, int columnIndex) {
-			return (this.context instanceof WritableManyValuedContext);
+			return (context instanceof WritableManyValuedContext);
 		}
 
 		public Class getColumnClass(int columnIndex) {
@@ -53,13 +63,13 @@ public class TableView extends JTable {
 		public Object getValueAt(int rowIndex, int columnIndex) {
 			FCAElement object = getObjectForRow(rowIndex);
 			ManyValuedAttribute attribute = getAttributeForColumn(columnIndex);
-			return this.context.getRelationship(object, attribute);
+			return context.getRelationship(object, attribute);
 		}
 
 		public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
 			FCAElement object = getObjectForRow(rowIndex);
 			ManyValuedAttribute attribute = getAttributeForColumn(columnIndex);
-			WritableManyValuedContext writableContext = (WritableManyValuedContext) this.context;
+			WritableManyValuedContext writableContext = (WritableManyValuedContext) context;
 			writableContext.setRelationship(object, attribute, (AttributeValue) aValue);
 		}
 
@@ -69,20 +79,49 @@ public class TableView extends JTable {
 		}
 
 		private FCAElement getObjectForRow(int rowIndex) {
-			return (FCAElement) this.context.getObjects().get(rowIndex);
+			return (FCAElement) context.getObjects().get(rowIndex);
 		}
 
 		private ManyValuedAttribute getAttributeForColumn(int columnIndex) {
-			return (ManyValuedAttribute) this.context.getAttributes().get(columnIndex);
+			return (ManyValuedAttribute) context.getAttributes().get(columnIndex);
 		}
 	}
 	
+	private class ContextTableColumnModle extends DefaultTableColumnModel {
+		
+	}
 	public TableView(ManyValuedContext context) {
-		setManyValuedContext(context);
+		this.context = context;
+		setModel(new ContextTableModel());
 		setAutoResizeMode(AUTO_RESIZE_OFF);
 	}
 
 	public void setManyValuedContext(ManyValuedContext context) {
-		setModel(new ContextTableModel(context));
+		this.context = context;
+		// code fragment picked from JTable.setModel() -- needed to update view
+		tableChanged(new TableModelEvent(dataModel, TableModelEvent.HEADER_ROW));
+	}
+	
+	public TableCellEditor getCellEditor(int row, int column) {
+		ManyValuedAttribute	mvAttr = (ManyValuedAttribute) this.context.getAttributes().get(column);
+		AttributeType type = mvAttr.getType();
+		if(type instanceof TextualType) {
+			TextualType textType = (TextualType) type;
+			JComboBox comp = new JComboBox(type.getValueRange());
+			return new DefaultCellEditor(comp);
+		}
+		if(type instanceof NumericalType) {
+			NumericalType numType = (NumericalType) type;
+			NumberFormat format = NumberFormat.getNumberInstance();
+			format.setMaximumFractionDigits(1);
+			final JFormattedTextField comp = new JFormattedTextField(new NumberFormatter(format));
+			return new DefaultCellEditor(comp) {
+				public Object getCellEditorValue() {
+					double value = Double.parseDouble(comp.getText());
+					return new NumericalValue(value);
+				}
+			};
+		}
+		throw new RuntimeException("Unknown table cell type");
 	}
 }
