b895dbe247af08a3643bb45f2cdf0a11820b6167
peterbecker <peterbecker@267469b6-68fb-4578-8a4b-5844dc4013a7>
2004-06-30 13:03:40 +0000

    Updated start configurations to include new libraries.
    git-svn-id: svn://svn.code.sf.net/p/toscanaj/code/trunk@1786 267469b6-68fb-4578-8a4b-5844dc4013a7

--- source/run-elba.bat
+++ source/run-elba.bat
@@ -1,1 +1,1 @@
-javaw -cp ToscanaJ.jar;libs/tockit-canvas.jar;libs/tockit-events.jar;libs/commons-cli.jar;libs/hsqldb.jar;libs/jdom.jar;libs/tockit-pluginArchitecture.jar;libs/tockit-relations.jar;libs/tockit-swing.jar;libs/tockit-utils.jar net.sourceforge.toscanaj.Elba
+javaw -cp ToscanaJ.jar;libs/tockit-canvas.jar;libs/tockit-events.jar;libs/commons-cli.jar;libs/hsqldb.jar;libs/jdom.jar;libs/tockit-pluginArchitecture.jar;libs/tockit-relations.jar;libs/tockit-swing.jar;libs/tockit-utils.jar;libs/tockit-fca.jar;libs/tockit-cernatoXML.jar net.sourceforge.toscanaj.Elba
