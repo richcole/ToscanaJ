8fd6eaf2f49684a47d6ea435c515836016a78152
peterbecker <peterbecker@267469b6-68fb-4578-8a4b-5844dc4013a7>
2004-07-25 03:57:45 +0000

    Removed the drop columns step when importing tuples from file.
    It was more confusing than useful. We could add dropping columns in the
    main view if we need to.
    
    
    git-svn-id: svn://svn.code.sf.net/p/toscanaj/code/trunk@1795 267469b6-68fb-4578-8a4b-5844dc4013a7

--- tupleware/source/org/tockit/tupleware/source/text/TextSource.java
+++ tupleware/source/org/tockit/tupleware/source/text/TextSource.java
@@ -19,7 +19,6 @@ import net.sourceforge.toscanaj.gui.dialog.ErrorDialog;
 import net.sourceforge.toscanaj.gui.dialog.ExtensionFileFilter;
 
 import org.tockit.relations.model.Relation;
-import org.tockit.relations.operations.DropColumnsOperation;
 import org.tockit.tupleware.gui.IndexSelectionDialog;
 import org.tockit.tupleware.source.TupleSource;
 
@@ -47,10 +46,7 @@ public class TextSource implements TupleSource {
                 if(this.tuples == null) {
                     throw new IOException("No tuples found in file");
                 }
-                IndexSelectionDialog dialog = new IndexSelectionDialog(parent, "Select columns to drop", this.tuples.getDimensionNames(), false);
-                dialog.show();
-                this.tuples = DropColumnsOperation.drop(this.tuples, dialog.getSelectedIndices());
-				dialog = new IndexSelectionDialog(parent, "Select object set", this.tuples.getDimensionNames());
+                IndexSelectionDialog dialog = new IndexSelectionDialog(parent, "Select object set", this.tuples.getDimensionNames());
 				dialog.show();
 				this.objectIndices = dialog.getSelectedIndices();
 			} catch (Exception e) {
