58c7d1810f2a19591a84c0bc91c112b55d3544da
peterbecker <peterbecker@267469b6-68fb-4578-8a4b-5844dc4013a7>
2008-09-17 01:32:34 +0000

    OAL export now support unassigned attributes.
    Any unassigned attribute (empty column in the context table) will be listed in the first line of the exported OAL file, which starts with a colon to mark the lack of an object.
    
    git-svn-id: svn://svn.code.sf.net/p/toscanaj/code/trunk@1978 267469b6-68fb-4578-8a4b-5844dc4013a7

--- toscanaj/source/net/sourceforge/toscanaj/writer/ObjectAttributeListWriter.java
+++ toscanaj/source/net/sourceforge/toscanaj/writer/ObjectAttributeListWriter.java
@@ -8,15 +8,43 @@
 package net.sourceforge.toscanaj.writer;
 
 import java.io.PrintStream;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 import org.tockit.context.model.Context;
 
 public class ObjectAttributeListWriter {
     public static void writeObjectAttributeList(final Context context,
             final PrintStream out) {
+        // first try and find attributes that are not assigned at all (empty
+        // columns)
+        final Set<Object> unassignedAttributes = new HashSet<Object>();
+        checkAttributeLoop: for (final Object attribute : context
+                .getAttributes()) {
+            for (final Object object : context.getObjects()) {
+                if (context.getRelation().contains(object, attribute)) {
+                    continue checkAttributeLoop;
+                }
+            }
+            unassignedAttributes.add(attribute);
+        }
+        if (unassignedAttributes.size() != 0) {
+            out.print(":");
+            boolean firstAttr = true;
+            for (final Object attribute : unassignedAttributes) {
+                if (firstAttr) {
+                    firstAttr = false;
+                } else {
+                    out.print(",");
+                }
+                out.print(attribute);
+
+            }
+            out.println();
+        }
         for (final Iterator<Object> objIt = context.getObjects().iterator(); objIt
-                .hasNext();) {
+        .hasNext();) {
             final Object object = objIt.next();
             out.print(object);
             out.print(":");
